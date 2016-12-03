a5b12f749b464575883d63e166392f65ff633c3f
Neil Mock <neilmock@gmail.com>
2016-12-02 19:32:26 -0500

    attempt fix

--- lib/fog/rackspace/storage.rb
+++ lib/fog/rackspace/storage.rb
@@ -119,8 +119,8 @@ module Fog
         # @return [Fog::Storage::Rackspace::Account] account details object
         def account
           account = Fog::Storage::Rackspace::Account.new(:service => self)
-          account.reload
           account.meta_temp_url_key = @rackspace_temp_url_key
+          account.reload
         end
       end
 
